71e9145d377ba99f26c0a08f4da13a50985f905e
jody@thescottsweb.com <jody@thescottsweb.com>
2019-12-30 16:32:39 -0600

    updated

--- install.sh
+++ install.sh
@@ -1,28 +1,29 @@
 #!/bin/sh
 
-main() {
-  cd $(dirname $0)
+main()
+{
+cd $(dirname $0)
 
-  thisdir=$(pwd | sed -E "s-^$HOME($|(/.*))-~\2-")
+thisdir=$(pwd | sed -E "s-^$HOME($|(/.*))-~\2-")
 
-  echo "PROFILE=$thisdir
-  source \${PROFILE}/zshrc" > ~/.zshrc
+echo "PROFILE=$thisdir
+source \${PROFILE}/zshrc" > ~/.zshrc
 
-  zshell=$(which zsh)
+zshell=$(which zsh)
 
-  [[ $(cat /etc/shells | grep "$zshell") ]] && {
-    err "Shell $zshell is in /etc/shells"
-  } || {
-    err "Shell $zshell is not in /etc/shells, adding. This will require sudo"
-    echo $zshell | sudo tee -a /etc/shells
-  }
+[[ $(cat /etc/shells | grep "$zshell") ]] && {
+  err "Shell $zshell is in /etc/shells"
+} || {
+  err "Shell $zshell is not in /etc/shells, adding. This will require sudo"
+  echo $zshell | sudo tee -a /etc/shells
+}
 
-  [[ "$SHELL" == "$zshell" ]] && {
-    err "User shell is $zshell";
-  } || {
-    err "User shell is $SHELL not $zshell; changing. This will require your password"
-    chsh -s $zshell
-  }
+[[ "$SHELL" == "$zshell" ]] && {
+  err "User shell is $zshell";
+} || {
+  err "User shell is $SHELL not $zshell; changing. This will require your password"
+  chsh -s $zshell
+}
 
 }
 
